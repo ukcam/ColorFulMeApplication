0b050a5629af208a9abed18f752d960cf1a05417
Macku Sanchez <mackusanchez@gmail.com>
2023-07-21 03:22:19 +0800

    Assessments with ishihara pointing logic

--- src/app/page/assessment-screen/assessment-screen.page.ts
+++ src/app/page/assessment-screen/assessment-screen.page.ts
@@ -15,38 +15,117 @@ import { Observable } from 'rxjs';
 })
 
 export class AssessmentScreenPage implements OnInit {
-  test1!: string;
-  test2!: string;
-  test3!: string;
-  test4!: string;
-  test5!: string;
-  test6!: string;
+  plate1!: string;
+  plate2!: string;
+  plate3!: string;
+  plate7!: string;
+  plate8!: string;
+  plate10!: string;
+  plate11!: string;
+  plate16!: string;
+  plate17!: string;
+  plate22!: string;
+  plate23!: string;
+  plate24!: string;
+  plate25!: string;
+
+  deuteran = 0
+  protan = 0
+  redGreen = 0
   total = 0
   isModalOpen = false;
+
+  resultString = ""
 
   constructor(private router: Router) { }
 
   ngOnInit() { }
 
   getScore() {
-    if (this.test1 === "1"){
+    if (this.plate1 === "12"){
       this.total = this.total + 1
     }
-    if (this.test2 === "2"){
+
+    if (this.plate2 === "8"){
       this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
     }
-    if (this.test3 === "3"){
+
+    if (this.plate3 === "6"){
       this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
     }
-    if (this.test4 === "4"){
+
+    if (this.plate7 === "3"){
       this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
     }
-    if (this.test5 === "5"){
+
+    if (this.plate8 === "15"){
       this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
     }
-    if (this.test6 === "6"){
+
+    if (this.plate10 === "2"){
       this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
     }
+
+    if (this.plate11 === "6"){
+      this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
+    }
+
+    if (this.plate16 === "16"){
+      this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
+    }
+
+    if (this.plate17 === "73"){
+      this.total = this.total + 1
+    } else {
+      this.redGreen = this.redGreen + 1
+    }
+
+    if (this.plate22 === "26"){
+      this.total = this.total + 1
+    } else if (this.plate22 === "6") {
+      this.protan = this.protan + 1
+    } else if (this.plate22 === "2") {
+      this.deuteran = this.deuteran + 1
+    }
+
+    if (this.plate23 === "42"){
+      this.total = this.total + 1
+    } else if (this.plate22 === "2") {
+      this.protan = this.protan + 1
+    } else if (this.plate22 === "4") {
+      this.deuteran = this.deuteran + 1
+    }
+
+    if (this.plate24 === "35"){
+      this.total = this.total + 1
+    } else if (this.plate22 === "5") {
+      this.protan = this.protan + 1
+    } else if (this.plate22 === "3") {
+      this.deuteran = this.deuteran + 1
+    }
+
+    if (this.plate25 === "96"){
+      this.total = this.total + 1
+    } else if (this.plate22 === "6") {
+      this.protan = this.protan + 1
+    } else if (this.plate22 === "9") {
+      this.deuteran = this.deuteran + 1
+    }
+
   }
 
   setOpen(isOpen: boolean) {
